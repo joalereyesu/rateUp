63aefc3d6ade57878a3971e0f7b288ed0b2a4c6e
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2021-11-26 00:14:01 -0600

    A

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import psycopg2
 
 app = Flask(__name__)
 
-con = psycopg2.connect(database="rateup", user="postgres", password="papalina", host="127.0.0.1", port="5432")
+con = psycopg2.connect(database="rateup", user="postgres", password="papalina", host="*", port="5432")
 print("Database opened successfully")
 
 
