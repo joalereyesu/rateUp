e5d2565d2bc1c1c626c56b181ffd3279b91546a7
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2021-11-26 02:22:37 -0600

    Nuevos tests

--- test_application.py
+++ test_application.py
@@ -3,11 +3,5 @@ with app.test_client() as c:
     response = c.get('/')
     assert response.status_code == 200
 
-with app.test_client() as c:
-    rv = c.post('/signUp', json={
-        'name': 'esteban', 'email': 'data', 'password': 'papalina'
-    })
-    json_data = rv.get_json()
-    assert rv.generate_response( json_data['resp'])
-    assert rv.status_code == 200
+
     
