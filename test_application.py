afc55a57fc371ea3a7a43fa5d60f22e9b7c8aff8
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2021-11-26 02:15:31 -0600

    Post

--- test_application.py
+++ test_application.py
@@ -2,3 +2,9 @@ from app import app
 with app.test_client() as c:
     response = c.get('/')
     assert response.status_code == 200
+
+with app.test_client() as c:
+    rv = c.post('/signUp', json={
+        'name': 'esteban', 'email': 'data', 'password': 'papalina'
+    })
+    assert response.status_code == 200
